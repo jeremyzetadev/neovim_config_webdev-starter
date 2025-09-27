2897edffdb960d6df01c481a927fdb09f5285b10
jeremyzetadev <jeremy_zeta@yahoo.com>
2025-09-27 18:47:14 +0900

    Update catpuccin.lua

--- lua/plugins/themes/catpuccin.lua
+++ lua/plugins/themes/catpuccin.lua
@@ -4,7 +4,20 @@ return
   name = "catppuccin",
   priority = 1000,
   config = function()
-    require("catppuccin").setup()
+          require("catppuccin").setup({
+          -- ... other configuration options ...
+          custom_highlights = function(colors)
+              return {
+                  -- This targets local variables using the standard Tree-sitter `@variable` group
+                   -- ["@variable"] = { fg = '#BD93F9' },
+                   ["@variable"] = { fg = colors.lavender },
+                  -- ["@parameter"] = { fg = colors.lavender },
+                  -- Add other overrides here
+                  -- Comment = { fg = colors.red}
+              }
+          end,
+      })
+
 
     -- setup must be called before loading
     vim.cmd.colorscheme "catppuccin"
